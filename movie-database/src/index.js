51a46b2c9f573d54ccd44666968fbcfabea72f6f
Aufa Billah <aufaroot18@gmail.com>
2022-04-12 14:34:44 +0700

    Add browser router in index

--- movie-database/src/index.js
+++ movie-database/src/index.js
@@ -9,13 +9,25 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+// Import Browser Router dari React Router
+import { BrowserRouter } from "react-router-dom";
+
 // get root element
 const container = document.getElementById("root");
 
 // create root
 const root = ReactDOM.createRoot(container);
 
-// render app to root
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    {/*
+     * Bungkus/Hubungkan Aplikasi dengan Browser Router.
+     * Agar URL browser dapat dimanipulasi oleh React Router
+     */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 reportWebVitals();

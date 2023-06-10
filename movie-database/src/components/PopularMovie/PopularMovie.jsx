d390ba11b3a33c060358fbf603433f88b998c243
Baihaki Al Biruni <baihakitanjung12@gmail.com>
2023-06-10 12:50:55 +0700

    updated

---
+++ movie-database/src/components/PopularMovie/PopularMovie.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+function PopularMovie() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios(URL);
+
+    setPopularMovies(response.data.results);
+  }, []);
+
+  return (
+    <div className="popular-movie">
+      <h2>Popular Movies</h2>
+      <div className="popular-movie__container">
+        {popularMovies.map((movie) => {
+          return (
+            <div className="popular-movie__card">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <h3>{movie.title}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
